a846fa77ce04f905a144bf3580e0818cf0b25ffb
mukeshsharma707 <mukeshsharma707@gmail.com>
2025-09-29 10:51:36 +0530

    minor changes done

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,9 +2,9 @@ import React from "react";
 const Hero=()=>{
     return (
         <>
-        <div style={{height:'270px', display:'flex', color:'white', alignItems:'center',justifyContent:'center', background:'black'}}>
-<div><h2><b>100 Thousand songs,ad-free<br/>Over thousands podcast episodes</b></h2></div>
-<div><img src="/images/vibrating-headphone.png" alt="Logo" /></div>
+        <div className="hero-section" style={{height:'270px', display:'flex', color:'white', alignItems:'center',justifyContent:'center', background:'black'}}>
+<h1>100 Thousand songs,ad-free<br/>Over thousands podcast episodes</h1>
+<img src="/images/vibrating-headphone.png" alt="Logo" />
 
         </div>
         </>
